a123eb757a6eb369c7c54ea4ac62990914d9971d
Maria Bianca Sousa <96076727+Bia14k@users.noreply.github.com>
2023-05-17 16:44:07 -0300

    Add files via upload

---
+++ Postgres/consultas_simulado.sql
@@ -0,0 +1,13 @@
+select count(country_name)
+from country;
+
+select * from ranking_system;
+
+select * from university;
+select count(university_name)
+from university;
+
+select * from university_year;
+
+select avg(student_staff_ratio)
+from university_year;
